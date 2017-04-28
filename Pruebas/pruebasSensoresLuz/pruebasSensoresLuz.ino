6eabadb74b44c579a7f43496097b48352c248621
gfdgeras <gfdgeras@gmail.com>
2017-04-28 11:16:38 -0700

    Pruebas sensores de luz

---
+++ Pruebas/pruebasSensoresLuz/pruebasSensoresLuz.ino
@@ -0,0 +1,39 @@
+#include <QTRSensors.h>
+
+
+
+
+#define NUM_SENSORS             6  
+#define NUM_SAMPLES_PER_SENSOR  4  
+#define EMITTER_PIN             2  
+
+
+QTRSensorsAnalog qtra((unsigned char[]) {0, 1, 2, 3, 4, 5}, 
+  NUM_SENSORS, NUM_SAMPLES_PER_SENSOR, EMITTER_PIN);
+unsigned int sensorValues[NUM_SENSORS];
+
+
+void setup()
+{
+  delay(500);
+  Serial.begin(9600); // set the data rate in bits per second for serial data transmission
+  delay(1000);
+}
+
+
+void loop()
+{
+  // read raw sensor values
+  qtra.read(sensorValues);
+  
+  // print the sensor values as numbers from 0 to 1023, where 0 means maximum reflectance and
+  // 1023 means minimum reflectance
+  for (unsigned char i = 0; i < NUM_SENSORS; i++)
+  {
+    Serial.print(sensorValues[i]);
+    Serial.print('\t'); // tab to format the raw data into columns in the Serial monitor
+  }
+  Serial.println();
+  
+  delay(250);
+}
